749007392355b6dacc317f4c9e5ff94b5d6590b0
kaaaaajin <119267797+kaaaaajin@users.noreply.github.com>
2022-12-31 13:43:33 +0000

    [add]follow

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,16 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   
+  has_many :relationships, foreign_key: :following_id #フォローする側からのhas_many
+  # relationshipsだけだとフォローする側からなのかされる側なのかわからないのでforeign_keyを設定
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: :follower_id　 #フォローされる側からのhas_many
+  # フォローする側からのアソシエーションと区別するためにreverse_of_relationships
+  # class_name: 'Relationship'の記述がないと、railsはreverse_of_relationshipsテーブルを探す
+  
+  has_many :followings, through: :relationships, source: :follower　#中間テーブルを介してフォローされる人を取ってくる
+  # あるユーザーがフォローしている人を全員取ってくる（フォロー一覧）
+  has_many :followers,　through: :reverse_of_relationships, source: :following
+  # あるユーザーをフォローしてくれている人を全員取ってくる（フォロワー一覧）
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
